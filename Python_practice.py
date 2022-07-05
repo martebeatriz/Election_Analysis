51835415faf8184ab222f3d31aec12098cc278c9
Beatriz <owner1@MacBook-Air.local>
2022-07-04 20:06:00 -0400

    Project

--- Python_practice.py
+++ Python_practice.py
@@ -24,4 +24,32 @@ voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, {"county":"De
 for countyVoters in voting_data:
     print(f"{countyVoters['county']} county has {countyVoters['registered_voters']} registered voters.")
      
-        
+
+
+# Initialize a total vote counter.
+total_votes = 0
+
+# Candidate Options and candidate votes.
+candidate_options = []
+candidate_votes = {}
+
+# 1: Create a county list and county votes dictionary.
+county_names = []
+county_votes = {}
+
+
+# Track the winning candidate, vote count and percentage
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+# 2: Track the largest county and county voter turnout.
+largest_county = ""
+county_voter_turnout = 0
+
+# Get the candidate name from each row.
+candidate_name = row[2]
+
+# 3: Extract the county name from each row.
+county_name = row[1]
+      
